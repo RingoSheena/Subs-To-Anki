cfbb370c3a4d528fdd6fa3b8d405fbd78429e508
Riley <swifterwatcher20@gmail.com>
2026-05-06 14:14:03 -0400

    Initial commit

--- src/main/java/com/riley/s2a/App.java
+++ src/main/java/com/riley/s2a/App.java
@@ -1,9 +1,5 @@
-package com.riley;
-
-/**
- * Hello world!
- *
- */
+package com.riley.s2a;
+ 
 public class App 
 {
     public static void main( String[] args )
---
+++ src/main/java/com/riley/s2a/SubtitleBlock.java
@@ -0,0 +1,5 @@
+package com.riley.s2a;
+
+public class SubtitleBlock {
+    
+}
